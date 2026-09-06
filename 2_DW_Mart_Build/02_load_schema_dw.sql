5dd4b3a25dfc43c32ee4450e1259e5cae82bb953
Aminul Islam <aminulislam96a@gmail.com>
2026-09-06 14:36:09 -0400

    load schemas and created mastar build script

---
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -0,0 +1,37 @@
+--Step2:DW -load data from CSV files into tables 
+
+--loading data into the company_dim table
+SELECT '--Loading company_dim table' as info;
+
+INSERT INTO company_dim(company_id, name)
+SELECT company_id, name
+FROM read_csv('https://storage.googleapis.com/sql_de/company_dim.csv',
+    AUTO_DETECT=true);
+
+--loading data into the skills_dim table
+SELECT '--Loading skills_dim  table' as info;
+
+INSERT INTO skills_dim (skill_id,skills,type)
+SELECT skill_id,skills,type
+FROM read_csv('https://storage.googleapis.com/sql_de/skills_dim.csv',
+    AUTO_DETECT=true);
+--loading data into the job_postings_fact table
+SELECT '--Loading job_postings_fact table' as info;
+
+INSERT INTO job_postings_fact (job_id, company_id,job_title_short,job_title,job_country,job_location,job_no_degree_mention,
+    job_posted_date,job_schedule_type,job_via,job_work_from_home,job_health_insurance,
+    search_location,salary_rate,salary_year_avg,salary_hour_avg
+)
+SELECT job_id, company_id,job_title_short,job_title,job_country,job_location,job_no_degree_mention,
+    job_posted_date,job_schedule_type,job_via,job_work_from_home,
+    job_health_insurance,search_location,salary_rate,salary_year_avg,salary_hour_avg
+FROM read_csv('https://storage.googleapis.com/sql_de/job_postings_fact.csv',
+    AUTO_DETECT=true);
+
+--loading data into the skills_job_dim table
+SELECT '--Loading skills_job_dim  table' as info;
+
+INSERT INTO skills_job_dim( skill_id,job_id)
+SELECT skill_id,job_id
+FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv',
+    AUTO_DETECT=true);
