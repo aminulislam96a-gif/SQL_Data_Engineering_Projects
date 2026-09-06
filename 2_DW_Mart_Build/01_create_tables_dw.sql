2ebd546bfa882dd1a8e6f7f81313eac276e3cf2e
Aminul Islam <aminulislam96a@gmail.com>
2026-09-06 12:50:28 -0400

    craete star scheam tables for data warehouse

--- 2_DW_Mart_Build/01_create_tables_dw.sql
+++ 2_DW_Mart_Build/01_create_tables_dw.sql
@@ -1,1 +1,50 @@
 -- Step 1: DW -Create star schema tables
+DROP TABLE IF EXISTS skills_job_dim;
+DROP TABLE IF EXISTS job_postings_fact;
+DROP TABLE IF EXISTS skills_dim;
+DROP TABLE IF EXISTS company_dim;
+--Create company_dim table 
+CREATE TABLE company_dim(
+    company_id          INTEGER         PRIMARY KEY,
+    name                VARCHAR
+); 
+--Create skills_dim table
+CREATE TABLE skills_dim(
+    skill_id            INTEGER PRIMARY KEY,
+    skills              VARCHAR,
+    type                VARCHAR
+);
+
+--Create job_postings_fact table 
+CREATE TABLE job_postings_fact (
+    job_id              INTEGER         PRIMARY KEY,
+    company_id          INTEGER,
+    job_title_short     VARCHAR,
+    job_title           VARCHAR,
+    job_location        VARCHAR,
+    job_via             VARCHAR,
+    job_schedule_type   VARCHAR,
+    job_work_from_home  BOOLEAN,
+    search_location     VARCHAR,
+    job_posted_date     TIMESTAMP,
+    job_no_degree_mention BOOLEAN,
+    job_health_insurance BOOLEAN,
+    job_country         VARCHAR,
+    salary_rate         VARCHAR,
+    salary_year_avg DOUBLE,
+    salary_hour_avg DOUBLE,
+    FOREIGN KEY(company_id) REFERENCES company_dim(company_id)
+);
+--Create skills_job_dim table
+CREATE TABLE skills_job_dim(
+    job_id               INTEGER,
+    skill_id             INTEGER,
+    PRIMARY KEY(job_id,skill_id),
+    FOREIGN KEY (job_id)   REFERENCES job_postings_fact(job_id),
+    FOREIGN KEY (skill_id) REFERENCES skills_dim(skill_id)
+);
+
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'main';
